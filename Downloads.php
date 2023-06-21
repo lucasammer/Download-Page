58cd9a9f3ecb597110c95fd4b72f604d79b3e2c6
Bread <77288737+lucasammer@users.noreply.github.com>
2023-06-21 15:27:23 +0200

    added styling

--- Downloads.php
+++ Downloads.php
@@ -1,12 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Downloads</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
 <?php
-// SETUP
-$FILES_LOCATION = "./files";
+    // SETUP
+    $FILES_LOCATION = "./files";
 
-// CODE
-$files = scandir($FILES_LOCATION);
+    // CODE
+    $files = scandir($FILES_LOCATION);
 
-for($i = 2; $i < count($files); $i++){
-    echo("<a href=\"$FILES_LOCATION/" . $files[$i] . "\" download>" . $files[$i] . "</a><br>");
-}
-
+    for($i = 2; $i < count($files); $i++){
+        echo("<a href=\"$FILES_LOCATION/" . $files[$i] . "\" download>" . $files[$i] . "</a><br>");
+    }
 ?>
+</div>
+</body>
+</html>
+
