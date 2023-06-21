a1c2975a33e8de68de904705b37af2634a14e8fa
Bread <77288737+lucasammer@users.noreply.github.com>
2023-06-21 15:36:24 +0200

    added Downloads text

--- Downloads.php
+++ Downloads.php
@@ -6,17 +6,18 @@
 </head>
 <body>
 <div class="container">
-<?php
-    // SETUP
-    $FILES_LOCATION = "./files";
+    <h1>Downloads</h1>
+    <?php
+        // SETUP
+        $FILES_LOCATION = "./files";
 
-    // CODE
-    $files = scandir($FILES_LOCATION);
+        // CODE
+        $files = scandir($FILES_LOCATION);
 
-    for($i = 2; $i < count($files); $i++){
-        echo("<a href=\"$FILES_LOCATION/" . $files[$i] . "\" download>" . $files[$i] . "</a><br>");
-    }
-?>
+        for($i = 2; $i < count($files); $i++){
+            echo("<a href=\"$FILES_LOCATION/" . $files[$i] . "\" download>" . $files[$i] . "</a><br>");
+        }
+    ?>
 </div>
 </body>
 </html>
